4359ae4e8c3c74ac37289138f87a33a4e3bb80cb
JevonsAn <anyu.hao@163.com>
2019-12-19 23:26:28 +0800

    complete d3-directed

--- src/components/d3-directed.vue
+++ src/components/d3-directed.vue
@@ -7,50 +7,130 @@
 <script>
   import * as d3 from 'd3';
 
+
   export default {
     name: "d3-directed",
+    props: {
+      links: Array,
+      nodes: Array,
+    },
     data: function () {
       return {
         svg_width: 700,
         svg_height: 700,
-        nodes: [{"asn":1, "value":2}, {"asn":2, "value":2}, {"asn":3, "value":4}],
-        links: [{"source":1, "target": 2}, {"source":1, "target": 3}],
       }
     },
     computed: {
-
-    },
-    methods: {
-      create_links: function () {
-        let svg = d3.select("svg#topo");
-        console.log(svg);
-        svg.append("circle");
-        svg.append("g")
-          .attr("class", "links")
-          .selectAll("line")
-          .data(this.links)
-          .enter().append("line")
-          .attr("stroke-width", d => Math.max(Math.min(4, 0.7* Math.sqrt(0.4*d.value)), 0.6))
-          .attr("value", d=>d.value)
-          // .attr('marker-end',function(d) {
-          //   if (d.shangye == "P2C") return "";
-          //   else return 'url(#arrowhead)';
-          //   })
-          // .attr("marker-start",function(d) {
-          //   if (d.shangye == "C2P") return "";
-          //   else return 'url(#arrowhead2)';
-          //   })
-          .attr("id",function (d) {
-            return "as" + d.source + "as" + d.target;
-          });
+      data_merge: function () {
+        return {
+          "links": this.links,
+          "nodes": this.nodes
+        }
       }
     },
+    methods: {
+      draw: function () {
+        const simulation = d3.forceSimulation(this.nodes)
+          .force("link", d3.forceLink(this.links).id(d => d.id))
+          .force("charge", d3.forceManyBody())
+          .force("center", d3.forceCenter(this.svg_width / 2, this.svg_height / 2));
+
+        const svg = d3.select("svg#topo");
+
+        const link = svg.append("g")
+          .attr("stroke", "#999")
+          .attr("stroke-opacity", 0.6)
+          .selectAll("line")
+          .data(this.links)
+          .join("line")
+          .attr("stroke-width", d => Math.sqrt(d.value));
+
+        const scale = d3.scaleOrdinal(d3.schemeCategory10);
+        const node = svg.append("g")
+          .attr("stroke", "#fff")
+          .attr("stroke-width", 1.5)
+          .selectAll("circle")
+          .data(this.nodes)
+          .join("circle")
+          .attr("r", 5)
+          .attr("fill", d => scale(d.group))
+          .call(drag(simulation));
+
+        node.append("title")
+          .text(d => d.id);
+
+        simulation.on("tick", () => {
+          link
+            .attr("x1", d => d.source.x)
+            .attr("y1", d => d.source.y)
+            .attr("x2", d => d.target.x)
+            .attr("y2", d => d.target.y);
+
+          node
+            .attr("cx", d => d.x)
+            .attr("cy", d => d.y);
+        });
+
+        // invalidation.then(() => simulation.stop());
+        // return svg.node();
+      },
+
+    },
+    watch: {
+      data_merge: function () {
+        this.draw();
+      },
+    },
     mounted() {
-      this.create_links()
+      this.draw();
     }
+
   }
+
+  let drag = simulation => {
+
+    function dragstarted(d) {
+      if (!d3.event.active) simulation.alphaTarget(0.3).restart();
+      d.fx = d.x;
+      d.fy = d.y;
+    }
+
+    function dragged(d) {
+      d.fx = d3.event.x;
+      d.fy = d3.event.y;
+    }
+
+    function dragended(d) {
+      if (!d3.event.active) simulation.alphaTarget(0);
+      d.fx = null;
+      d.fy = null;
+    }
+
+    return d3.drag()
+      .on("start", dragstarted)
+      .on("drag", dragged)
+      .on("end", dragended);
+  };
+
 </script>
 
 <style scoped>
 
+</style>
+
+<style>
+  .link line {
+    stroke: #333;
+    stroke-opacity: 0.6;
+  }
+  /*.node circle {
+    stroke: #333;
+    stroke-width: 1px;
+    stroke-opacity: 0.6;
+  }*/
+  .node{
+    stroke: #333;
+    stroke-width: 1px;
+    stroke-opacity: 0.6;
+  }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,21 @@
 <template>
   <div class="home">
-    <directed></directed>
+    <directed :links="links" :nodes="nodes"></directed>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import directed from '../components/d3-directed'
-
+import data_json from "../assets/miserables"
 export default {
   name: 'home',
+  data: function () {
+    return {
+      links: data_json.links,
+      nodes: data_json.nodes,
+    }
+  },
   components: {
     directed
   }
